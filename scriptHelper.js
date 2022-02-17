a6d53bd3a8f75d8b7378f5c93d2258243e209384
Allyson Wilson <allysonwilson@Allysons-MBP.lan>
2022-02-17 00:03:18 -0600

    This is the first part of the assignment

--- scriptHelper.js
+++ scriptHelper.js
@@ -17,11 +17,56 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
 }
 
 function validateInput(testInput) {
-   
+    let transformation = Number(testInput);
+
+   if (transformation == ""){
+        return "Empty";
+   }
+
+    else if (isNaN(transformation)) {
+        return "Not a Number";
+}
+    else if (isNaN(transformation) == false) {
+        return "Is a Number";
+    }
 }
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-   
+
+    if (validateInput(pilot) === "Empty" || (validateInput(copilot) === "Empty") || (validateInput(fuelLevel) === "Empty") || (validateInput(cargoLevel) === "Empty")) {
+        alert ("All fields are required!");
+    }
+
+    if (validateInput(pilot) === "Is a Number" || (validateInput(copilot) === "Is a Number") || (validateInput(fuelLevel === "Not a Number") || (validateInput(cargoLevel) === "Not a Number"))) {
+        alert ("Make sure to enter valid information for each field!");
+    }
+
+    document.getElementById ("pilotStatus").innerHTML =  `Pilot ${pilot} is ready for launch.`;
+    document.getElementById ("copilotStatus").innerHTML =  `Copilot ${copilot} is ready for launch.`;
+
+    if (fuelLevel < 10000 && cargoLevel <= 10000) {
+        list.style.visibility = "visible";
+        document.getElementById ("fuelStatus").innerHTML = "Fuel Level is too low for launch.";
+        document.getElementById ("cargoStatus").innerHTML = "Cargo Mass is low enough to launch.";
+        document.getElementById ("launchStatus").innerHTML = "Shuttle is not ready for launch.";
+        document.getElementById ("launchStatus").style.color = "red";
+    }
+
+    if (cargoLevel > 10000 && fuelLevel >= 10000) {
+        list.style.visibility = "visible";
+        document.getElementById ("cargoStatus").innerHTML = "Cargo Mass is too heavy for takeoff.";
+        document.getElementById ("fuelStatus").innerHTML = "Fuel level is sufficient for launch."
+        document.getElementById ("launchStatus").innerHTML = "Shuttle not ready for launch.";
+        document.getElementById ("launchStatus").style.color = "red";
+    }
+
+    if (fuelLevel >= 10000 && cargoLevel <= 10000) {
+        list.style.visibility = "visible";
+        document.getElementById ("launchStatus").innerHTML = "Shuttle is ready for launch";
+        document.getElementById ("lauchStatus").style.color = "green";
+        document.getElementById ("fuelStatus").innerHTML = "Fuel level is sufficient for launch.";
+        document.getElementById ("cargoStatus").innerHTML = "Cargo Mass is low enough for launch.";
+    }
 }
 
 async function myFetch() {
